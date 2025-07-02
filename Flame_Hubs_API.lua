f1ec6bacf6757f211f8038f79ad96874f3613027
FlamesHub_Official <165463731+EnterpriseExperience@users.noreply.github.com>
2025-07-02 16:17:58 -0400

    Update Flame_Hubs_API.lua

--- Flame_Hubs_API.lua
+++ Flame_Hubs_API.lua
@@ -1,3 +1,24 @@
+-- Usage:
+
+-- local flames_api = loadstring(game:HttpGet("https://raw.githubusercontent.com/EnterpriseExperience/MicUpSource/refs/heads/main/Flames_API.lua"))()
+
+-- Flames API arguments:
+-- flames_api.FindPlayer("random") -- example of 'FindPlayer', supports: shortening names, displaynames, and usernames. (does not let you target yourself, will be fixed soon, had some issues with that part).
+-- flames_api.ExecutorName -- will let you display the executor's current name (just name, not version), works for TextLabels and stuff to, automatically defaults to "Unknown" if somehow not supported or not found, and is automatically a tostring.
+-- flames_api.Service("Service_Name") -- Service getter, will obviously check if the service even exists before getting it, but will default to "cloneref" sandboxing the service if supported, if not somehow, it'll default to regular service fetcher.
+-- flames_api.LocalPlayer -- Should be obvious what this does, get's your LocalPlayer.
+-- flames_api.RandomString(string) -- when inputting a string it'll randomize it for you automatically.
+-- flames_api.HumanoidRootPart -- grabs your HumanoidRootPart (even if you die, respawn, etc).
+-- flames_api.Humanoid -- grabs your Humanoid (even if you die, respawn, etc).
+-- flames_api.Head -- grabs your Head (even if you die, respawn, etc).
+-- flames_api.SeatPart -- grabs your current SeatPart.
+-- flames_api.Vehicle -- uses "SeatPart" to get your Vehicle, and checks the Vehicle to see if it has wheels, and if it's a Model as well.
+-- flames_api.GoTo -- usage: flames_api.GoTo("Player") and it will attempt to Pivot your Character to they're Character if the target name is correct and exists.
+-- flames_api.WalkSpeed -- Allows you to set your WalkSpeed in this API.
+-- flames_api.JumpPower -- Allows you to set your JumpHeight/JumpPower in this API, has detection for JumpHeight AND JumpPower, so if your game uses JumpPower, it'll know, and if JumpHeight, it'll know.
+-- flames_api.BypassWalkSpeed -- Will attempt to use 'getconnections', if supported, to bypass WalkSpeed checks from games (this method only works if the game detects using "GetPropertyChangedSignal")
+-- flames_api.BypassJumpPower -- Will attempt to use 'getconnections', if supported, to bypass JumpHeight/JumPower checks from games (this method only works if the game detects using "GetPropertyChangedSignal")
+
 local flames_api = {}
 
 local function SafeGet(service)
@@ -66,6 +87,7 @@ local function Dynamic_Character_Updater(character)
 end
 
 flames_api.Character = LocalPlayer.Character or LocalPlayer.CharacterAdded:Wait()
+local Character = LocalPlayer.Character or LocalPlayer.CharacterAdded:Wait()
 Dynamic_Character_Updater(flames_api.Character)
 
 LocalPlayer.CharacterAdded:Connect(function(newCharacter)
@@ -76,7 +98,7 @@ LocalPlayer.CharacterAdded:Connect(function(newCharacter)
 	flames_api.Humanoid = SafeGetHumanoid(newCharacter)
 	flames_api.HumanoidRootPart = SafeGetHRP(newCharacter)
 	flames_api.Head = SafeGetHead(newCharacter)
-	flames_api.SeatPart = flames_api.Humanoid and flames_api.Humanoid.SeatPart or nil
+	flames_api.SeatPart = Humanoid.SeatPart
 	wait(0.2)
 	Dynamic_Character_Updater(newCharacter)
 end)
@@ -91,11 +113,11 @@ flames_api.Vehicle = function()
 		local model = seatPart:FindFirstAncestorOfClass("Model")
 
 		if model and typeof(model.GetDescendants) == "function" then
-				for _, descendant in ipairs(model:GetDescendants()) do
-					if descendant:IsA("BasePart") and descendant.Name and string.find(descendant.Name:lower(), "wheel") then
-						return model
-					end
+			for _, descendant in ipairs(model:GetDescendants()) do
+				if descendant:IsA("BasePart") and descendant.Name and string.find(descendant.Name:lower(), "wheel") then
+					return model
 				end
+			end
 		end
 	end
 
@@ -104,17 +126,18 @@ end
 
 flames_api.FindPlayer = function(arg)
 	local tbl = Players:GetPlayers()
+	local cmdlp = Players.LocalPlayer
 
 	if args == "me" or args == cmdlp.Name or args == cmdlp.DisplayName then
-		return getgenv().notify("Failure!", "You cannot target yourself!", 6)
+		return warn("[ERROR]:", "You cannot target yourself.")
 	end
 
 	if args == "random" then
 		local validPlayers = {}
 		for _, v in pairs(tbl) do
-				if v ~= cmdlp then
-					table.insert(validPlayers, v)
-				end
+			if v ~= cmdlp then
+				table.insert(validPlayers, v)
+			end
 		end
 		return #validPlayers > 0 and validPlayers[math.random(1, #validPlayers)] or nil
 	end
@@ -122,9 +145,9 @@ flames_api.FindPlayer = function(arg)
 	if args == "new" then
 		local vAges = {}
 		for _, v in pairs(tbl) do
-				if v.AccountAge < 30 and v ~= cmdlp then
-					table.insert(vAges, v)
-				end
+			if v.AccountAge < 30 and v ~= cmdlp then
+				table.insert(vAges, v)
+			end
 		end
 		return #vAges > 0 and vAges[math.random(1, #vAges)] or nil
 	end
@@ -132,9 +155,9 @@ flames_api.FindPlayer = function(arg)
 	if args == "old" then
 		local vAges = {}
 		for _, v in pairs(tbl) do
-				if v.AccountAge > 30 and v ~= cmdlp then
-					table.insert(vAges, v)
-				end
+			if v.AccountAge > 30 and v ~= cmdlp then
+				table.insert(vAges, v)
+			end
 		end
 		return #vAges > 0 and vAges[math.random(1, #vAges)] or nil
 	end
@@ -142,9 +165,9 @@ flames_api.FindPlayer = function(arg)
 	if args == "bacon" then
 		local vAges = {}
 		for _, v in pairs(tbl) do
-				if v ~= cmdlp and (v.Character:FindFirstChild("Pal Hair") or v.Character:FindFirstChild("Kate Hair")) then
-					table.insert(vAges, v)
-				end
+			if v ~= cmdlp and (v.Character:FindFirstChild("Pal Hair") or v.Character:FindFirstChild("Kate Hair")) then
+				table.insert(vAges, v)
+			end
 		end
 		return #vAges > 0 and vAges[math.random(1, #vAges)] or nil
 	end
@@ -152,9 +175,9 @@ flames_api.FindPlayer = function(arg)
 	if args == "friend" then
 		local friendList = {}
 		for _, v in pairs(tbl) do
-				if v:IsFriendsWith(cmdlp.UserId) and v ~= cmdlp then
-					table.insert(friendList, v)
-				end
+			if v:IsFriendsWith(cmdlp.UserId) and v ~= cmdlp then
+				table.insert(friendList, v)
+			end
 		end
 		return #friendList > 0 and friendList[math.random(1, #friendList)] or nil
 	end
@@ -162,9 +185,9 @@ flames_api.FindPlayer = function(arg)
 	if args == "notfriend" then
 		local vAges = {}
 		for _, v in pairs(tbl) do
-				if not v:IsFriendsWith(cmdlp.UserId) and v ~= cmdlp then
-					table.insert(vAges, v)
-				end
+			if not v:IsFriendsWith(cmdlp.UserId) and v ~= cmdlp then
+				table.insert(vAges, v)
+			end
 		end
 		return #vAges > 0 and vAges[math.random(1, #vAges)] or nil
 	end
@@ -172,9 +195,9 @@ flames_api.FindPlayer = function(arg)
 	if args == "ally" then
 		local vAges = {}
 		for _, v in pairs(tbl) do
-				if v.Team == cmdlp.Team and v ~= cmdlp then
-					table.insert(vAges, v)
-				end
+			if v.Team == cmdlp.Team and v ~= cmdlp then
+				table.insert(vAges, v)
+			end
 		end
 		return #vAges > 0 and vAges[math.random(1, #vAges)] or nil
 	end
@@ -182,9 +205,9 @@ flames_api.FindPlayer = function(arg)
 	if args == "enemy" then
 		local vAges = {}
 		for _, v in pairs(tbl) do
-				if v.Team ~= cmdlp.Team and v ~= cmdlp then
-					table.insert(vAges, v)
-				end
+			if v.Team ~= cmdlp.Team and v ~= cmdlp then
+				table.insert(vAges, v)
+			end
 		end
 		return #vAges > 0 and vAges[math.random(1, #vAges)] or nil
 	end
@@ -192,16 +215,16 @@ flames_api.FindPlayer = function(arg)
 	if args == "near" then
 		local vAges = {}
 		for _, v in pairs(tbl) do
-				if v ~= cmdlp and v.Character and cmdlp.Character then
-					local vRootPart = v.Character:FindFirstChild("HumanoidRootPart")
-					local cmdlpRootPart = cmdlp.Character:FindFirstChild("HumanoidRootPart")
-					if vRootPart and cmdlpRootPart then
-						local distance = (vRootPart.Position - cmdlpRootPart.Position).magnitude
-						if distance < 30 then
-								table.insert(vAges, v)
-						end
+			if v ~= cmdlp and v.Character and cmdlp.Character then
+				local vRootPart = v.Character:FindFirstChild("HumanoidRootPart")
+				local cmdlpRootPart = cmdlp.Character:FindFirstChild("HumanoidRootPart")
+				if vRootPart and cmdlpRootPart then
+					local distance = (vRootPart.Position - cmdlpRootPart.Position).magnitude
+					if distance < 30 then
+						table.insert(vAges, v)
 					end
 				end
+			end
 		end
 		return #vAges > 0 and vAges[math.random(1, #vAges)] or nil
 	end
@@ -209,24 +232,115 @@ flames_api.FindPlayer = function(arg)
 	if args == "far" then
 		local vAges = {}
 		for _, v in pairs(tbl) do
-				if v ~= cmdlp and v.Character and cmdlp.Character then
-					local vRootPart = v.Character:FindFirstChild("HumanoidRootPart")
-					local cmdlpRootPart = cmdlp.Character:FindFirstChild("HumanoidRootPart")
-					if vRootPart and cmdlpRootPart then
-						local distance = (vRootPart.Position - cmdlpRootPart.Position).magnitude
-						if distance > 30 then
-								table.insert(vAges, v)
-						end
+			if v ~= cmdlp and v.Character and cmdlp.Character then
+				local vRootPart = v.Character:FindFirstChild("HumanoidRootPart")
+				local cmdlpRootPart = cmdlp.Character:FindFirstChild("HumanoidRootPart")
+				if vRootPart and cmdlpRootPart then
+					local distance = (vRootPart.Position - cmdlpRootPart.Position).magnitude
+					if distance > 30 then
+						table.insert(vAges, v)
 					end
 				end
+			end
 		end
 		return #vAges > 0 and vAges[math.random(1, #vAges)] or nil
 	end
 
 	for _, v in pairs(tbl) do
 		if (v.Name:lower():find(args:lower()) or v.DisplayName:lower():find(args:lower())) and v ~= cmdlp then
-				return v
+			return v
 		end
+	end
+end
+wait(0.1)
+flames_api.GoTo = function(playerArg)
+	local success, err = pcall(function()
+		local Target = flames_api.FindPlayer(playerArg)
+		if not Target then return end
+
+		if Target.Character and flames_api.Character then
+			local TargetChar = Target.Character or Target.CharacterAdded:Wait()
+			task.wait(0.1)
+			flames_api.Character:PivotTo(TargetChar:GetPivot())
+		end
+	end)
+	if not success then
+		warn("[GoTo_Error]: " .. tostring(err))
+	end
+end
+
+flames_api.WalkSpeed = function(Speed)
+	if type(Speed) ~= "number" then return end
+	pcall(function()
+		if flames_api.Humanoid then
+			flames_api.Humanoid.WalkSpeed = Speed
+		end
+	end)
+end
+
+flames_api.JumpPower = function(JP)
+	if type(JP) ~= "number" then return end
+	pcall(function()
+		if flames_api.Humanoid.UseJumpPower or flames_api.Humanoid.UseJumpPower == true then
+			flames_api.Humanoid.JumpPower = JP
+		else
+			flames_api.Humanoid.JumpHeight = JP
+		end
+	end)
+end
+
+flames_api.BypassWalkSpeed = function()
+	if not getconnections then
+		return warn("[ERROR]:", "Your executor does not support 'getconnections'!")
+	end
+
+	local humanoid = flames_api.Humanoid
+
+	if not humanoid then
+		return warn("[ERROR]:", "Humanoid not found inside Character at runtime.")
+	end
+
+	local signal = humanoid:GetPropertyChangedSignal("WalkSpeed")
+	local connections = getconnections(signal)
+	for _, conn in ipairs(connections) do
+		pcall(function()
+			conn:Disable()
+		end)
+	end
+end
+
+flames_api.BypassJumpPower = function()
+	if not getconnections then
+		return warn("[ERROR]:", "Your executor does not support 'getconnections'!")
+	end
+
+	local humanoid = flames_api.Humanoid
+
+	if not humanoid then
+		return warn("[ERROR]:", "Humanoid not found inside Character at runtime.")
+	end
+
+	local signal = humanoid:GetPropertyChangedSignal("JumpHeight")
+	local connections = getconnections(signal)
+	for _, conn in ipairs(connections) do
+		pcall(function()
+			conn:Disable()
+		end)
+	end
+	local signal = humanoid:GetPropertyChangedSignal("UseJumpPower")
+	local connections = getconnections(signal)
+	for _, conn in ipairs(connections) do
+		pcall(function()
+			conn:Disable()
+		end)
+	end
+	wait()
+	local signal = humanoid:GetPropertyChangedSignal("JumpPower")
+	local connections = getconnections(signal)
+	for _, conn in ipairs(connections) do
+		pcall(function()
+			conn:Disable()
+		end)
 	end
 end
 
