bf5154773d8754218821f5f259358644fa82fe0a
FlamesHub_Official <165463731+EnterpriseExperience@users.noreply.github.com>
2025-09-17 18:39:36 -0400

    Update LifeTogether_RP_Admin.lua

--- LifeTogether_RP_Admin.lua
+++ LifeTogether_RP_Admin.lua
@@ -2300,6 +2300,9 @@ end
 
 local function disable_rgb_for(plr)
    if not plr then return warn("Player was not found when trying to disable RGB vehicle!") end
+   if not getgenv().VehicleStates[plr.Name] then return end
+   if not getgenv().Rainbow_Tasks[plr.Name] then return end
+
    if getgenv().VehicleStates[plr.Name].rainbow == true then
       notify("Disabling:", "Vehicle states for: "..tostring(plr.Name), 3)
       getgenv().VehicleStates[plr.Name].rainbow = false
