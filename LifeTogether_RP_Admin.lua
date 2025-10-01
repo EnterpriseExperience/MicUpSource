f7d8268609771e29c2492a492c6cc75839fd75c4
FlamesHub_Official <165463731+EnterpriseExperience@users.noreply.github.com>
2025-10-01 14:57:07 -0400

    Update LifeTogether_RP_Admin.lua

--- LifeTogether_RP_Admin.lua
+++ LifeTogether_RP_Admin.lua
@@ -5654,40 +5654,6 @@ end)
 wait()
 local Players = getgenv().Players or cloneref(game:GetService("Players"))
 local LocalPlayer = getgenv().LocalPlayer or Players.LocalPlayer
-local Handle_Error = getgenv().Error_API and getgenv().Error_API.Handle_Error
-wait()
-getgenv().Reporting_Errors = true
-
-local function ReportError(msg, trace)
-   if not Handle_Error then return end
-   local fullMessage = tostring(msg) .. "\n" .. tostring(trace or debug.traceback())
-   Handle_Error(LocalPlayer, fullMessage)
-end
-
-local function SafeWrap(fn)
-   return function(...)
-      return xpcall(fn, function(err)
-         ReportError(err, debug.traceback())
-         return nil
-      end, ...)
-   end
-end
-
-getgenv().ScriptContext.Error:Connect(function(message, stackTrace, scriptInstance)
-   ReportError(message, stackTrace)
-end)
-
-task.spawn(function()
-   while getgenv().Reporting_Errors == true do
-      task.wait(1)
-      local ok, err = pcall(function()
-         
-      end)
-      if not ok and err then
-         ReportError(err, debug.traceback())
-      end
-   end
-end)
 task.wait(0.2)
 function Notify(message, duration)
    local function safe_wrapper(S)
