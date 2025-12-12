eba1ff53f1d256f9fffb62c62f19856f355d74e0
FlamesHub_Official <165463731+EnterpriseExperience@users.noreply.github.com>
2025-12-11 23:23:59 -0500

    Update LifeTogether_RP_Admin.lua

--- LifeTogether_RP_Admin.lua
+++ LifeTogether_RP_Admin.lua
@@ -4,13 +4,13 @@ local function load_script_from_url(url)
     end)
 
     if not success or not code or code == "" then
-        warn("Failed to fetch script from: " .. url)
+        warn("Failed to fetch script from: "..tostring(url))
         return false
     end
 
     local func, err = loadstring(code)
     if not func then
-        warn("Loadstring failed: " .. tostring(err))
+        warn("Loadstring failed: "..tostring(err))
         return false
     end
 
@@ -20,3 +20,4 @@ local function load_script_from_url(url)
 end
 
 load_script_from_url("https://raw.githubusercontent.com/EnterpriseExperience/MicUpSource/refs/heads/main/LifeTogether_Admin_Obf.lua")
+
