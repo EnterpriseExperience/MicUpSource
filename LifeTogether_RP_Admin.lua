2c5565e1331d1e31c675cb75a91b7cb6bf5d5018
FlamesHub_Official <165463731+EnterpriseExperience@users.noreply.github.com>
2025-12-11 23:03:02 -0500

    Update LifeTogether_RP_Admin.lua

--- LifeTogether_RP_Admin.lua
+++ LifeTogether_RP_Admin.lua
@@ -1,14 +1,3 @@
-local urls = {
-    "https://raw.githubusercontent.com/EnterpriseExperience/MicUpSource/refs/heads/main/LifeTogether_Admin_Obf.lua",
-    "https://pastebin.com/raw/azPSzEjH",
-    "https://pastefy.app/SiDMhe47/raw"
-}
-
-local httpget =
-	(typeof and typeof(game.HttpGet) == "function" and game.HttpGet) or
-	(typeof and typeof(game.HttpGetAsync) == "function" and game.HttpGetAsync) or
-	request or http_request or (syn and syn.request) or (http and http.request) or (fluxus and fluxus.request)
-
 local function load_script(code)
     if code and type(code) == "string" then
         local f, err = loadstring(code)
@@ -22,29 +11,4 @@ local function load_script(code)
     return false
 end
 
-local success = false
-
-for _, url in ipairs(urls) do
-    local ok, body = pcall(game.HttpGet, game, url, true)
-    if ok and type(body) == "string" and body ~= "" then
-        if load_script(body) then
-            success = true
-            break
-        end
-    end
-
-    if httpget then
-        local req_ok, res = pcall(syn.request, {Url = url, Method = "GET"})
-        if req_ok and res and res.Success and res.Body then
-            print("Loaded via httprequest: "..url)
-            if load_script(res.Body) then
-                success = true
-                break
-            end
-        end
-    end
-end
-
-if not success then
-    warn("Failed to load script from all sources.")
-end
+load_script("https://raw.githubusercontent.com/EnterpriseExperience/MicUpSource/refs/heads/main/LifeTogether_Admin_Obf.lua")
