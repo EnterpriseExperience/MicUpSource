2e7ab127615ca4fb69d770d28b6e64f9b2aff0c6
FlamesHub_Official <165463731+EnterpriseExperience@users.noreply.github.com>
2025-12-24 18:51:57 -0500

    Create SupportedGames.lua

---
+++ SupportedGames.lua
@@ -0,0 +1,50 @@
+getgenv().current_supported_flames_hub_experiences = {
+    ["Tower Of Misery"] = {
+        id = 4954752502,
+        link = "https://raw.githubusercontent.com/EnterpriseExperience/MicUpSource/refs/heads/main/Tower_Of_Misery_ScriptHub.lua"
+    },
+    ["Ultimate Driving"] = {
+        id = 54865335,
+        link = "https://raw.githubusercontent.com/EnterpriseExperience/MicUpSource/refs/heads/main/Ultimate_Driving_Hub.lua"
+    },
+    ["LifeTogether RP"] = {
+        id = {13967668166, 99644611200703, 99154507657228},
+        link = "https://raw.githubusercontent.com/EnterpriseExperience/MicUpSource/refs/heads/main/LifeTogether_RP.lua"
+    },
+    ["LifeTogether Admin Commands (FE)"] = {
+        id = {13967668166, 99644611200703, 99154507657228},
+        link = "https://raw.githubusercontent.com/EnterpriseExperience/MicUpSource/refs/heads/main/LifeTogether_RP_Admin.lua"
+    },
+    ["Hide And Seek Extreme"] = {
+        id = 205224386,
+        link = "https://raw.githubusercontent.com/EnterpriseExperience/MicUpSource/refs/heads/main/Hide_And_Seek_Extreme.lua"
+    },
+    ["Player or AI"] = {
+        id = 95217169945642,
+        link = "https://raw.githubusercontent.com/EnterpriseExperience/MicUpSource/refs/heads/main/Player_Or_AI.lua"
+    },
+    ["Main Street RP"] = {
+        id = 18753889337,
+        link = "https://raw.githubusercontent.com/EnterpriseExperience/MicUpSource/refs/heads/main/Main_Street_RP.lua"
+    },
+    ["Southwest Florida Beta"] = {
+        id = 5104202731,
+        link = "https://raw.githubusercontent.com/EnterpriseExperience/MicUpSource/refs/heads/main/Southwest_Florida.lua"
+    },
+    ["Driving Empire"] = {
+        id = 3351674303,
+        link = "https://raw.githubusercontent.com/EnterpriseExperience/MicUpSource/refs/heads/main/Driving_Empire.lua"
+    },
+    ["Berry Avenue RP"] = {
+        id = 8481844229,
+        link = "https://raw.githubusercontent.com/EnterpriseExperience/MicUpSource/refs/heads/main/Berry_Ave_Admin.lua"
+    },
+    ["Mega Fun Obby"] = {
+        id = 12996397,
+        link = "https://raw.githubusercontent.com/EnterpriseExperience/MicUpSource/refs/heads/main/MegaFunObby.lua"
+    },
+    ["Catalog Avatar Creator"] = {
+        id = 7041939546,
+        link = "https://raw.githubusercontent.com/EnterpriseExperience/MicUpSource/refs/heads/main/CatalogAvatarCreator.lua"
+    }
+}
