05020082654056cbde0877155fadedc6484d340a
FlamesHub_Official <165463731+EnterpriseExperience@users.noreply.github.com>
2026-03-11 19:35:52 -0400

    Update zacks_easy_module.lua

--- zacks_easy_module.lua
+++ zacks_easy_module.lua
@@ -20,7 +20,7 @@ local Teams
 local CoreGui
 local StarterGui
 local SoundService
-local Version = "10.7.5"
+local Version = "10.7.8"
 
 function unload_services()
     getgenv().getLoopKick = false
