495bbfc991e2b8a145e0beeb6235424a799da0d8
FlamesHub_Official <165463731+EnterpriseExperience@users.noreply.github.com>
2026-04-14 23:56:43 -0400

    Update LifeTogether_RP.lua

--- LifeTogether_RP.lua
+++ LifeTogether_RP.lua
@@ -1,6 +1,1 @@
-if not game:IsLoaded() then game.Loaded:Wait() end
-
-local NotifyLib = loadstring(game:HttpGet("https://raw.githubusercontent.com/EnterpriseExperience/MicUpSource/refs/heads/main/Notification_Lib.lua"))()
-wait(0.1)
-NotifyLib:External_Notification("Warning", "This script hub has been deprecated and is no longer available, running the new Admin script.", 30)
-loadstring(game:HttpGet('https://raw.githubusercontent.com/EnterpriseExperience/MicUpSource/refs/heads/main/LifeTogether_RP_Admin.lua'))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/EnterpriseExperience/MicUpSource/refs/heads/main/LifeTogether_Admin_Obf.lua"))()
